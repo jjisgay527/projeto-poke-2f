202f59384adc43ef8b323250973c6cf76d74e672
JP <rm95193@estudante.fieb.edu.br>
2025-05-16 12:59:21 +0000

    Flex commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,24 @@
-
+import poke from './assets/pokeapi_256 1.png'
+import eevee from './assets/eevee.jpg'
+import './Home.css'
 
 const Home =()=>{
+
     return (
         <div>
-            <p>Routes blood Routes</p>
-        </div>
+        <img src={poke} />
+        <img scr={eevee} />
+        <img src={poke} />
+        <img scr={eevee} />
+        <img src={poke} />
+        <img scr={eevee} />
+        <img src={poke} />
+        <img scr={eevee} />
+        <img src={poke} />
+        <img scr={eevee} />
+        <img src={poke} />
+        <img scr={eevee} />
+    </div>
     )
 }
 
